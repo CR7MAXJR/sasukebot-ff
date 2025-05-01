b266a9cefc25d796909c9486555a7966cb385b2a
CR7MAXJR <mohammedhatim490@gmail.com>
2025-05-01 22:11:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
  
-say('BY:KAKASHI', {
+say('BY:غيو ثرونز', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
